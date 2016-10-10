9cf68f733ff503d1abcc63f8bb8af7b39955deaf
Joseph <joseph@verifiedjoseph.com>
2016-10-10 21:50:54 +0100

    Removed admin class php

--- code/404.php
+++ code/404.php
@@ -3,21 +3,7 @@
 header("HTTP/1.0 404 Not Found");
 
 include 'config.php';
-include 'classes/admin_class.php';
-	
-	$admin = new admin();
-	
-	$messages = array();
-	$channel_id = 'UCBa659QWEk1AI4Tg--mrJ2A'; // Channel ID
-	
-	// Get File 
-	if(!file_exists('gs://#default#/locations.json')) { // Check if file exists
-		$messages[] = "locations.json file not found.";
-		$current_file ="locations.json file not found.";
-	} else {
-		// Fetch theme details file (details.json)
-		$current_file = file_get_contents($root_url . '/file');
-	}	
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
